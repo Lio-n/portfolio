dacb68afb419edc678be04af37f99578d7f422e4
Lion <leonardofontan14@gmail.com>
2023-04-26 15:10:08 -0300

    feat : add @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <main style={{ display: "grid", gap: "10rem" }}>
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </>
   );
